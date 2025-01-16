5e8c204b8dbe88cbda4a2d66027de7367842291b
KarumeMaina <karumejoe7@gmail.com>
2025-01-16 16:16:07 +0300

    update reg log fun & UI for register

--- app/src/main/java/com/example/firebasecrudapp/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/firebasecrudapp/viewmodel/UserViewModel.kt
@@ -1,12 +1,32 @@
 package com.example.firebasecrudapp.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.firebasecrudapp.repository.FirebaseRepository
+import kotlinx.coroutines.launch
 
 class UserViewModel: ViewModel {
     private val repositoey = FirebaseRepository()
 
-    fun registerUser(name:String, email:String, password:String, nResult: (Boolean, String?) -> Unit) {
+    fun registerUser(name: String, email: String, password: String, onResult: (Boolean, String?) -> Unit) {
+        viewModelScope.launch {
+            try {
+                val userId = repository.registerUser(name, email, password)
+                onResult(true, userId)
+            } catch (e: Exception) {
+                onResult(false, e.message)
+            }
+        }
+    }
 
-    })
+    fun loginUser(email: String, password: String, onResult: (Boolean, String?) -> Unit){
+        viewModelScope.launch {
+            try {
+                val userId = repository.loginUser(email, password)
+                onResult(true, userId)
+            } catch (e: Exception) {
+                onResult(false, e.message)
+            }
+        }
+    }
 }
