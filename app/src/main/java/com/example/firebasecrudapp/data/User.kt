591f37b6b9df7d252e0252938f0337f81efaed01
KarumeMaina <karumejoe7@gmail.com>
2025-01-16 15:52:34 +0300

    adding Data model class

---
+++ app/src/main/java/com/example/firebasecrudapp/data/User.kt
@@ -0,0 +1,10 @@
+package com.example.firebasecrudapp.data
+
+
+
+data class User(
+    val id: String? = null,
+    val name: String = "",
+    val email: String = "",
+    val password: String = ""
+)
